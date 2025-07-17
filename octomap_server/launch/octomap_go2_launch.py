8fc4eca2a2f5f54be03795c4d8f08da6647ed600
Shubham <52372631+skpawar1305@users.noreply.github.com>
2025-07-17 10:45:07 -0300

    create octomap for go2

---
+++ octomap_server/launch/octomap_go2_launch.py
@@ -0,0 +1,46 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    node_list = []
+
+    octomap_server = Node(
+        package='octomap_server',
+        executable='octomap_server_node',
+        output='screen',
+        parameters=[{
+            "resolution": 0.05,
+            "frame_id": "map",
+            "base_frame_id": "base_link",
+            "sensor_model.max_range": 2.0,
+            "latch": False,
+            "exploration": True,
+            "multiple_pointclouds": False,
+        }],
+        remappings=[
+            ("cloud_in", "/utlidar_lidar"),
+        ],
+    )
+    node_list.append(octomap_server)
+
+    map_1m = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "1", "0", "0", "0", "map", "map_1m"],
+    )
+    node_list.append(map_1m)
+
+    map_2m = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        output="screen",
+        arguments=["0", "0", "2", "0", "0", "0", "map", "map_2m"],
+    )
+    node_list.append(map_2m)
+
+    return LaunchDescription(node_list)
